0694653ebfcd6de8dc9397e2e259c80d1011a5c6
Andronicus Lepcha <79708526+AndronicusLepcha@users.noreply.github.com>
2022-04-11 20:49:57 +0530

    Abstract implementation in java
    The Abstract in java is the process of hiding the implementation details ans showing functionality to the user.  We cannot create the object of the abstract class

---
+++ Abstract.java
@@ -0,0 +1,23 @@
+import java.util.*;
+abstract class A{
+	abstract void display();
+	 void show(){
+		System.out.println("This is inside the abstract class A");
+		}	
+	}
+
+class B  extends A{
+		void display(){
+		System.out.println("I am defining the abstract method in Sub class B");
+		
+		}
+}
+
+class Abstract{
+	public static void main(String args[]){
+		//	A a=new A();  This is not allowed or we cannot create the object of the abstract class
+			B b=new B();
+			b.display();
+			b.show();
+		}
+}
