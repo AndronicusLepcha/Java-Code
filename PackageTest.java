55b75104c9af5a55f3d6cc11a39c8e2c95e66611
Andronicus Lepcha <79708526+AndronicusLepcha@users.noreply.github.com>
2022-04-11 21:00:48 +0530

    Package in JAVA
    Implementing the concept of package in JAVA

---
+++ PackageTest.java
@@ -0,0 +1,13 @@
+import Mypackage.PackA;
+import Mypackage.PackB;
+import Mypackage.PackC;
+class PackageTest{
+	public static void main(String args[]){
+			PackA aob=new PackA();
+			PackB bob=new PackB();
+			PackC cob=new PackC();
+			aob.show();
+			bob.show();
+			cob.show();
+		}
+}
