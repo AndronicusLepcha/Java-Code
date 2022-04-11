fdab24786584f04cdfe1d3edd4c5ecf8dce6f93d
Andronicus Lepcha <79708526+AndronicusLepcha@users.noreply.github.com>
2022-04-11 21:07:46 +0530

    User Information Updated.

--- PackageTest.java
+++ PackageTest.java
@@ -1,3 +1,42 @@
+/*
+//create Package as follows:
+
+package Mypackage;
+
+public class PackA{
+        public void show(){
+        System.out.println("\nThis is the Package of PackA Class!");
+        }
+}
+package Mypackage;
+
+public class PackB{
+        public void show(){
+        System.out.println("\nThis is the Package of PackB Class!");
+        }
+}
+
+package Mypackage;
+
+public class PackC{
+        public void show(){
+        System.out.println("\nThis is the Package of PackC Class!");
+        }
+}
+
+/*Now first compile your pacakge by  javac -d . package_name
+One dir will be created as Mypackage;
+move to Mypackage dir and ls
+you can see the different class file there
+And Then Execute the following the code*/
+
+
+*/
+
+
+
+
+
 import Mypackage.PackA;
 import Mypackage.PackB;
 import Mypackage.PackC;
